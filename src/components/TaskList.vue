9e44d7759ef885e08483526d1f10a489d22dbd10
snagasawa <s.nagasawa.github@gmail.com>
2019-12-06 04:04:01 +0900

    Extract to functions

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div>
-      <input type="text" v-model="state.taskName" />
+      <input type="text" v-model="taskNameRef" />
       <button @click="addTask">Add</button>
     </div>
     <div>
-      <input type="text" v-model="state.searchText" />Search
+      <input type="text" v-model="searchTextRef" />Search
     </div>
     <div class="task-list-wrapper">
       <ul>
         <h4>DOING</h4>
-        <li v-for="(task, index) in state.doingTasks" :key="index">
+        <li v-for="(task, index) in doingTasks" :key="index">
           <input type="checkbox" :checked="task.status" disabled/>
           <label>{{ task.name }}</label>
           <button @click="toggleTask(task, true)">toggle</button>
@@ -18,7 +18,7 @@
       </ul>
       <ul>
         <h4>COMPLETED</h4>
-        <li v-for="(task, index) in state.completedTasks" :key="index">
+        <li v-for="(task, index) in completedTasks" :key="index">
           <input type="checkbox" :checked="task.status" disabled/>
           <label>{{ task.name }}</label>
           <button @click="toggleTask(task, false)">toggle</button>
@@ -29,36 +29,94 @@
 </template>
 
 <script>
-import { reactive, computed } from '@vue/composition-api';
+import { computed, watch, ref, isRef } from '@vue/composition-api';
 
+const useTaskList = () => {
+  const tasksRef = ref([]);
+
+  const toggleTask = (task, status) => {
+    const index = tasksRef.value.indexOf(task);
+    tasksRef.value.splice(index, 1, { ...task, status: status });
+  };
+
+  return {
+    tasksRef,
+    toggleTask,
+  }; 
+};
+
+const useAddingTask = (tasksRef) => {
+  const taskNameRef = ref('');
+
+  const addTask = () => {
+    tasksRef.value.push({
+      name: taskNameRef.value,
+      status: false,
+    });
+    taskNameRef.value = '';
+  }
+
+  return {
+    taskNameRef,
+    addTask,
+  };
+};
+
+const useFilter = (tasks = []) => {
+  const tasksRef = isRef(tasks) ? tasks : ref(tasks);
+  const valid = Array.isArray(tasksRef.value);
+
+  const doingTasks = valid ?
+    computed(() => tasksRef.value.filter(t => !t.status)) :
+    () => { return [] };
+  const completedTasks = valid ?
+    computed(() => tasksRef.value.filter(t => t.status)):
+    () => { return [] };
+
+  return {
+    doingTasks,
+    completedTasks,
+  };
+};
+
+const useSearcher = (tasks = []) => {
+  const searchTextRef = ref('');
+  const tasksRef = ref(tasks);
+  const valid = Array.isArray(tasksRef.value);
+  
+  const search = valid ?
+    computed(() => tasksRef.value.filter(t => t.name.includes(searchTextRef.value))) :
+    () => { return [] };
+
+  return {
+    searchTextRef,
+    search,
+  };
+};
+ 
 export default {
   setup() {
-    const state = reactive({
-      taskName: '',
-      searchText: '',
-      tasks: [],
-      doingTasks: computed(() => state.searchedTasks.filter(t => !t.status)),
-      completedTasks: computed(() => state.searchedTasks.filter(t => t.status)),
-      searchedTasks: computed(() => state.tasks.filter(t => t.name.includes(state.searchText))),
-    });
+    const { tasksRef, toggleTask } = useTaskList();
+    const { taskNameRef, addTask } = useAddingTask(tasksRef);
+    const { searchTextRef, search } = useSearcher(tasksRef.value);
+    const { doingTasks, completedTasks } = useFilter(search); 
 
-    function addTask() {
-      state.tasks.push({
-        name: state.taskName,
-        status: false,
-      });
-      state.taskName = '';
-    }
-
-    function toggleTask(task, status) {
-      const index = state.tasks.indexOf(task);
-      state.tasks.splice(index, 1, { ...task, status: status });
-    }
+    watch([doingTasks, completedTasks], () => {
+      console.log('doingTasks: ', doingTasks.value);
+      console.log('completedTasks: ', completedTasks.value);
+    })
 
     return {
-      state,
+      // Mutable state
+      tasksRef,
+      taskNameRef,
+      searchTextRef,
+      // Functions
       addTask,
-      toggleTask
+      toggleTask,
+      // Computed
+      doingTasks,
+      completedTasks,
     }
   }
 }
